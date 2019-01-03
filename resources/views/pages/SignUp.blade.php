ae0c5dc108c6c841ab7ae35fb04b702b673f4ff4
TurmericSama <loperapaulo@gmail.com>
2019-01-03 15:20:33 +0800

    3:20

--- resources/views/pages/SignUp.blade.php
+++ resources/views/pages/SignUp.blade.php
@@ -12,38 +12,52 @@
     @endif
 </head>
 <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+                <a style="color:green;" class="navbar-brand" href="/index.php">Appoint.me</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>            
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <button class="btn btn-outline-primary my-2 my-sm-0 ml-2" onclick="javascript: window.location='/login'">Login</button>
+                </div>
+            </nav>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12">
-                <div>Signup</div>
+            <div class="col-md-1">
+
+            </div>
+            <div class="col-md-11">
+                <div><h3 class="mt-3 mb-3 text-light">Signup</h3></div>
                 <div>
                     <form method="POST">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-row">
-                            <div class="form-group col-3">
+                            <div class="form-group col-2">
                                 <label for="username">Username</label>
-                                <input type="text" class="form-control" placeholder="Username" aria-label="Username" name="username">
+                                <input type="text" class="form-control" placeholder="Username" aria-label="Username" name="username" required>
                             </div>
-                            <div class="form-group col-3">
+                            <div class="form-group col-2">
                                 <label for="password">Password</label>
-                                <input type="password" class="form-control" placeholder="Password" aria-label="Username" name="password">
+                                <input type="password" class="form-control" placeholder="Password" aria-label="Username" name="password" required>
                             </div>
                             <div class="form-group col-3">
                                 <label for="E-mail">E-mail</label>
-                                <input type="text" class="form-control" placeholder="E-mail" aria-label="Username" name="email">
+                                <input type="text" class="form-control" placeholder="E-mail" aria-label="Username" name="email" required>
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="First Name" aria-label="Username" name="fname"><br>
+                            <div class="form-group col-3">
+                                <label for="fname">Firstname</label>
+                                <input type="text" class="form-control" placeholder="First Name" aria-label="Username" name="fname" required>
                             </div>
-                        </div>
-                        
-                        <div class="form-group w-25">
-                            <input type="text" class="form-control" placeholder="Middle Name" aria-label="Username" name="mname"><br>
-                        </div>
-                        <div class="form-group w-25">
-                            <input type="text" class="form-control" placeholder="Last Name" aria-label="Username" name="lname"><br>
+                            <div class="form-group col-3">
+                                <label for="mname">Middlename</label>
+                                <input type="text" class="form-control" placeholder="Middle Name" aria-label="Username" name="mname" required>
+                            </div>
+                            <div class="form-group col-3">
+                                <label for="lname">Lastname</label>
+                                <input type="text" class="form-control" placeholder="Last Name" aria-label="Username" name="lname" required>
+                            </div>
                         </div>
                         <input type="submit" value="Create Account" class="btn btn-primary">
                     </form>
