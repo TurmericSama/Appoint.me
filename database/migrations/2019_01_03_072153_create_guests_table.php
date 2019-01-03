e8cc3d36e7dd665f330b8b7b807d782d786aa49d
unidentifiedpy <unidentifiedpy@github.com>
2019-01-03 15:45:08 +0800

    change>

---
+++ database/migrations/2019_01_03_072153_create_guests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer( "appointment_id" )->references( "id" )->on( "appointments" );
+            $table->integer( "user_id" )->references( "id" )->on( "users" );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
