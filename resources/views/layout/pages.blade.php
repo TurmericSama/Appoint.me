13253318147b3b7171ae3e053b748fc1fd9620e1
TurmericSama <loperapaulo@gmail.com>
2019-01-08 15:18:31 +0800

    sdf

--- resources/views/layout/pages.blade.php
+++ resources/views/layout/pages.blade.php
@@ -8,6 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--links and scripts-->
 <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/toastr.min.css')}}">
 <link rel="stylesheet" href="{{asset('css/style.css')}}">
 <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
 <script src="{{asset('js/jquery.form.js')}}"></script>
@@ -15,7 +16,6 @@
 <script src="{{asset('js/popper.min.js')}}"></script>
 <script src="{{asset('js/toastr.min.js')}}"></script>
 <script src="{{asset('js/jquery.form.js')}}"></script>
-<link rel="stylesheet" href="{{asset('css/toastr.min.css')}}">
 </head>
 <body>
     
